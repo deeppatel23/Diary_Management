4a9aa090bb2e3ba9e9751af876db995ca7233ac0
Deep Patel <62326316+deeppatel23@users.noreply.github.com>
2021-01-21 19:26:21 +0530

    Update urls.py

--- diary/diary/urls.py
+++ diary/diary/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#admin deeppatel2.2312@gmail.com d#diary
 
 from django.contrib import admin
 from django.urls import path, include
